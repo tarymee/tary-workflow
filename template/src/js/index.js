5a407500645355bdc16c50e2a345b297ecd870cb
tarymee <tarymee@gmail.com>
2017-08-23 18:37:44 +0800

    修改js

--- template/src/js/index.js
+++ template/src/js/index.js
@@ -1,27 +1,3 @@
-var getJSON = function(url) {
-    return new Promise((resolve, reject) => {
-        var client = new XMLHttpRequest();
-        client.open("GET", url);
-        client.onreadystatechange = handler;
-        client.responseType = "json";
-        client.setRequestHeader("Accept", "application/json");
-        client.send();
-
-        function handler() {
-            if (this.readyState !== 4) {
-                return;
-            }
-            if (this.status === 200) {
-                resolve(this.response);
-            } else {
-                reject(new Error(this.statusText));
-            }
-        };
-    });
-};
-
-getJSON("/posts.json").then(function(json) {
-    console.log('Contents: ' + json);
-}, function(error) {
-    console.error('出错了', error);
-});
+console.log('Hello World')
+console.log('Hello World')
+console.log('Hello World')
